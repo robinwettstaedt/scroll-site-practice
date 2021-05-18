f73b5b9abd9d4243680c23409aab750a239ff5a5
robinwettstaedt <robinwettstadt@gmail.com>
2021-05-18 21:01:32 +0200

    sort of got the mobile dropdown menu working now

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
-// ********** set date ************
-// select span
-// const date = document.getElementById("date");
-// date.innerHTML = new Date().getFullYear();
-
-// // ********** close links ************
-// const navToggle = document.querySelector(".nav-toggle");
-// const linksContainer = document.querySelector(".links-container");
-// const links = document.querySelector(".links");
-
-// navToggle.addEventListener("click", function() {
-//     // linksContainer.classList.toggle("show-links");
-
-//     const linksHeight = links.getBoundingClientRect().height;
-//     const containerHeight = linksContainer.getBoundingClientRect().height;
-//     if (containerHeight === 0) {
-//         linksContainer.style.height = `${linksHeight}px`;
-//     } else {
-//         linksContainer.style.height = 0;
-//     }
-//     // console.log(linksContainer.getBoundingClientRect());
-// });
-
 // ********** fixed navbar ************
 
 const navbar = document.querySelector('nav');
@@ -33,6 +5,9 @@ const barsIcon = document.querySelector('#bars-icon');
 const logoIcon = document.querySelector('#logo-icon');
 const navLinks = [...document.querySelectorAll('#nav-links')];
 const clientHeight = window.innerHeight;
+const dropdownMenu = document.querySelector('.dropdown-list');
+const dropdownListItems = [...document.querySelectorAll('.dropdown-list-items')];
+let isDropped = false;
 
 window.addEventListener("scroll", function() {
     const scrollHeight = window.pageYOffset;
@@ -49,36 +24,39 @@ window.addEventListener("scroll", function() {
     }
 });
 
+barsIcon.addEventListener('click', function() {
+    if (isDropped) {
+        navbar.classList.remove("nav-menu-opened");
+        dropdownMenu.classList.remove("dropdown-list-opened");
+        barsIcon.classList.remove("bars-icon-menu-opened");
+        logoIcon.classList.remove("logo-icon-menu-opened");
+        dropdownListItems.map(item => item.classList.remove("dropdown-list-items-visible"));
+        isDropped = false;
 
-// ********** smooth scroll ************
-// select links
-// const scrollLinks = document.querySelectorAll(".scroll-link");
-// scrollLinks.forEach((link) => {
-//     link.addEventListener("click", (e) => {
-//         // prevent default
-//         e.preventDefault();
-//         // navigate to specific spot
-//         const id = e.currentTarget.getAttribute("href").slice(1);
-//         const element = document.getElementById(id);
+    } else {
+        navbar.classList.add("nav-menu-opened");
+        dropdownMenu.classList.add("dropdown-list-opened");
+        barsIcon.classList.add("bars-icon-menu-opened");
+        logoIcon.classList.add("logo-icon-menu-opened");
+        dropdownListItems.map(item => item.classList.add("dropdown-list-items-visible"));
+        isDropped = true;
 
-//         const navHeight = navbar.getBoundingClientRect().height;
-//         const containerHeight = linksContainer.getBoundingClientRect().height;
-//         const fixedNav = navbar.classList.contains("fixed-nav");
-//         let position = element.offsetTop - navHeight;
+    }
+})
 
-//         if (!fixedNav) {
-//             position = position - navHeight;
-//         }
-//         if (navHeight > 82) {
-//             position = position + containerHeight;
-//         }
+// barsIcon.addEventListener('click', function() {
+//     if (dropdownMenu.style.display === "block") {
+//         navbar.classList.remove("nav-menu-opened");
+//         dropdownMenu.style.display = "none";
+//         barsIcon.classList.remove("bars-icon-menu-opened");
+//         logoIcon.classList.remove("logo-icon-menu-opened");
 
-//         window.scrollTo({
-//             left: 0,
-//             top: position,
-//         });
-//         // close
-//         linksContainer.style.height = 0;
-//     });
-// });
-// calculate heights
+//     } else {
+//         navbar.classList.add("nav-menu-opened");
+//         dropdownMenu.style.display = "block";
+//         barsIcon.classList.add("bars-icon-menu-opened");
+//         logoIcon.classList.add("logo-icon-menu-opened");
+
+
+//     }
+// })
